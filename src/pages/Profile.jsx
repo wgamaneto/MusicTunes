829f7d5402b19541172faec75093ac778b51b1ce
washington gama <wgamaneto16@gmail.com>
2022-08-31 10:42:36 -0300

    requisito 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Loading from '../Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  state = {
+    loading: false,
+    people: {},
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true }, async () => {
+      const usuario = await getUser();
+      this.setState({ loading: false, people: usuario });
+    });
+  }
+
   render() {
+    const { loading, people } = this.state;
     return (
       <div data-testid="page-profile">
+        { loading ? <Loading /> : (
+          <>
+            <p>{people.name}</p>
+            <p>{people.email}</p>
+            <p>{people.description}</p>
+            <img
+              data-testid="profile-image"
+              src={ people.image }
+              alt="adamastor"
+            />
+            <Link to="/profile/edit">
+              Editar perfil
+            </Link>
+          </>
+        )}
         Profile
       </div>
     );
