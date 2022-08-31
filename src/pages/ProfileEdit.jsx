af5167615e89ea427bb6c872f856041effc04131
washington gama <wgamaneto16@gmail.com>
2022-08-31 10:51:04 -0300

    depois termino a 14

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// import Loading from '../Loading';
+// import { getUser } from '../services/userAPI';
 
 class ProfileEdit extends React.Component {
+  state = {
+    // loading: false,
+    // ximboca: {},
+  };
+
+  componentDidMount() {
+    // this.setState({ loading: true }, async () => {
+    //   const usuario = await getUser();
+    //   this.setState({ loading: false, people: usuario });
+    // });
+  }
+
   render() {
+    // const { loading, people } = this.state;
     return (
       <div data-testid="page-profile-edit">
         ProfileEdit
